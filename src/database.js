da49957083dbff8be4ad10dc819f5110e574ea64
황동규 <hdg2342@naver.com>
2023-06-23 00:05:45 +0900

    sss

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
-// const {PrismaClient} = require("@prisma/client")
-const { PrismaClient } = require('../node_modules/.prisma/client');
+const {PrismaClient} = require("@prisma/client")
+// const { PrismaClient } = require('../node_modules/.prisma/client');
 
 const prisma = new PrismaClient();
 
